76416de47088d22b2e5eb2a8d8776971e115ae91
Jiyoon Choi <jiyoonchoi15@gmail.com>
2024-09-30 21:39:00 -0400

    updated README

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl">Village Food Hub!</h1>
+    </div>
+  );
+}
