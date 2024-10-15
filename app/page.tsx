6f7ad76bcf7e593c90cc9ec153a77fd0687405a3
Emily Yuan <emyuans@yahoo.com>
2024-10-14 21:09:25 -0400

    Emily + Daniel button code

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import WeatherButton from './components/Button';
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl">Village Food Hub!</h1>
+      {/* <h1 className="text-4xl">Village Food Hub!</h1> */}
+      <WeatherButton label="Get Weather" />
     </div>
   );
 }
