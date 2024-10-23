7454405b8b34bbd522d94ae4882e51a471a3226a
Amanda Wu <amandawu@Amandas-MacBook-Air.local>
2024-10-22 20:27:55 -0400

    setting up banner

---
+++ app/components/DemographicSurveyBanner.tsx
@@ -0,0 +1,7 @@
+
+
+export default function DemographicSurveyBanner() {
+    return (
+        <div> Hello </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Header from "./components/DemographicSurveyBanner";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl">Village Food Hub!</h1>
-    </div>
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-4xl">Village Food Hub!</h1>
+      </div>
+      <Header/>
+    </>
   );
 }
