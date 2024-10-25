80bafbcfeee3b32dbf7e4ad0c26df5340d381c9e
Ella <yipingella369@gmail.com>
2024-10-25 15:00:24 -0400

    added scema to prisma db

---
+++ prisma/migrations/20241025185246_inventory/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "demographics" (
+    "phoneNumber" TEXT NOT NULL,
+    "takeCount" INTEGER NOT NULL,
+    "donateCount" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "householdSize" INTEGER NOT NULL,
+    "address" TEXT NOT NULL,
+    "lastVisitDate" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "demographics_pkey" PRIMARY KEY ("phoneNumber")
+);
+
+-- CreateTable
+CREATE TABLE "inventory" (
+    "itemName" TEXT NOT NULL,
+    "categoryName" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    "units" TEXT NOT NULL,
+    "lastUpdated" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "inventory_pkey" PRIMARY KEY ("itemName")
+);
