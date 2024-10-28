34b0cdeb9210d91901c39ac0a6ffd412d72db9c7
alexanderviolet <alexander.violet@tufts.edu>
2024-10-27 21:07:49 -0400

    This is an error

--- app/components/ProgressBars.tsx
+++ app/components/ProgressBars.tsx
@@ -1,3 +1,13 @@
 // This is where you will implement the progress bars (there are 5 total)
 
 // Write your code here
+"use client"
+
+import React from 'react';
+
+const ProgressBar = ({ value, max }) => {
+    return (
+        <progress value={value} max={max} style={{ width: '100%' }} />
+    );
+};
+export default ProgressBar;
--- app/customer-pages/welcome-page/[[...welcome-page]]/page.tsx
+++ app/customer-pages/welcome-page/[[...welcome-page]]/page.tsx
@@ -1,2 +1,33 @@
-// This is where you will call your components!
-// Write your code here
+"use client"
+
+// import React, { useState, useEffect } from 'react';
+// import Link from 'next/link';
+// import ProgressBar from '/app/components/ProgressBar';
+// import ProgressBar from '../../../components/ProgressBars';
+// export default function Welcome() () => {
+//     // const [progress, setProgress] = useState(0);
+//     // // Simulating progress updates
+//     // useEffect(() => {
+//     //     const interval = setInterval(() => {
+//     //         setProgress((prev) => (prev < 100 ? prev + 10 : prev));
+//     //     }, 1000);
+//     //     return () => clearInterval(interval);
+//     // }, []);
+//     return (
+//         <div>
+//             <h1>Welcome Page</h1>
+//             /* <ProgressBar value={70} max={100} />
+//             <p>Progress: {70}%</p> */
+//         </div>
+//     );
+// };
+// // export default Welcome;
+
+
+export default function Welcome() {
+    return (
+        <div>
+            <h1>Welcome Page</h1>
+        </div>
+    );
+}
