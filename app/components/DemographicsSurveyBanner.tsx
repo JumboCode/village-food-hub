955f14d8738fc44035e699c6d7235dc325eebebf
Amanda Wu <amandawu@Amandas-MacBook-Air.local>
2024-10-29 21:10:07 -0400

    wip

--- app/components/DemographicsSurveyBanner.tsx
+++ app/components/DemographicsSurveyBanner.tsx
@@ -1,4 +1,47 @@
-// This is where you will implement the green banner (demographics survey banner)
-// which will be displayed on the top of the page for the user to take the survey.
+import React from 'react';
+import Image from 'next/image'
+import whiteOutlineLogo from '../images/headerLogo.png'
 
-// Write your code here
+export default function Banner() {
+    const myStyles: React.CSSProperties = {
+        position: 'absolute',
+        width: '100%',
+        height: '150px',
+        left: '0px',
+        top: '0px',
+        background: '#24593D',
+        alignItems: 'center',
+        justifyContent: 'center',
+        display: 'flex',
+   };
+
+    const imageStyles: React.CSSProperties = {
+        marginRight: '20px',
+        position: 'absolute',
+        left: '0px',
+        bottom: '0px',
+    };
+    
+   const textStyles: React.CSSProperties =  {
+      fontSize: 64,
+      textAlign: 'center',
+      fontFamily: "'Crimson Text', serif",
+      fontWeight: '700',
+    };
+    
+    return ( 
+      <div style={myStyles} className="banner">
+        <div style={imageStyles}>
+            <Image
+                src={whiteOutlineLogo}
+                alt="logo"
+                width={196}
+                height={150}
+            />
+        </div>
+        <div className="banner-content">
+          <h1 style={textStyles}>Demographic Survey</h1>
+        </div>
+      </div>
+    );
+}
--- app/saved-thank-you/[[...saved-thank-you]]/page.tsx
+++ app/saved-thank-you/[[...saved-thank-you]]/page.tsx
@@ -1,2 +1,19 @@
 // This is where you will call your components!
 // Write your code here
+import React from 'react';
+import './page.css';
+// import '../../Components/DemographicsSurveyBanner.tsx';
+
+export default function SavedThankYou() {
+    // const pagestyle: React.CSSProperties = {
+    //         backgroundColor: "white",
+    // };
+    
+    return (
+        <div> 
+            <h1 className="header" >Thank you for saving your progress!</h1>
+        </div>
+        );
+}
+
+//make page white
