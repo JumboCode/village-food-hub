52ea3b72baa5622d47ad528e9dd5cb54ec51e982
Sofia A <alvazzi.sofia@gmail.com>
2024-10-29 21:36:50 -0400

    Last commit

--- app/api/demographics/route.ts
+++ app/api/demographics/route.ts
@@ -1,9 +1,9 @@
 // your route file for demographics
-import { PrismaClient } require ('@prisma/client');
+const { PrismaClient } = require ('@prisma/client');
 const prisma = new PrismaClient();
 
 //CREATE
-async function createDemographic(data : {
+async function createDemographic(data: {
     phoneNumber : string,
     takeCount: number,
     donateCount: number,
@@ -67,72 +67,6 @@ return await prisma.demographics.delete({
 }
 
 //export the functions
-module.exports = {createDemographic, getDemographic, updateDemographic, deleteDemographic};
+module.exports = {createDemographic, getDemographic, updateDemographic, 
+deleteDemographic};
 
-/* OLD CRUD FUNCTIONS:
-//CREATE (POST) creates a demographic entry
-async function createDemographic(req, res) { //data is the info we store
-    //tries to create new demographic entry in database
-    const data: DemographicData = req.body;
-    try {
-        //creates new row in demographics table
-        const newEntry = await prisma.demographics.create({
-            data,
-        });
-        return res.status(201).json(newEntry);
-    //error handling
-    } catch (error) {
-        console.error('Error creating demographic entry: ', error);
-        return res.status(400).json({ error: "Error creating demographic entry." });
-    }
-}
-
-//READ(get) retrieves demographics 
-async function getDemographic(req, res) {
-    try {
-        //gets all the records using findMany
-        const demographics = await prisma.demographics.findMany();  
-        //successful request and includes fetched records 
-        res.status(200).json(demographics); 
-    //error handling 
-    } catch (error) {
-        console.error('Error reading demographics ', error);
-        res.status(500).json({ error: "Error fetching demographic records." });
-    }
-}
-
-//UPDATE (PUT) changes informatoin about a demographic entry
-async function updateDemographic(phoneNumber, data: DemographicData) {
-    //takes phoneNumber to identify which entry, data: new values for entry
-    try {
-        const updatedEntry = await prisma.demographics.update({
-            where: { phoneNumber: phoneNumber }, //identifies entry to update
-            data, //new data to apply
-        });
-        return updatedEntry;
-    } catch (error) {
-        console.error('Error updating demographic entry: ', error);
-        throw error;
-    }
-}
-
-//DELETE (DELETE) removes a demographic entry
-async function deleteDemographic(req, res) {
-    const phoneNumber = req.params.phoneNumber; 
-
-    try {
-        // Delete the demographic entry with the given phoneNumber
-        await prisma.demographics.delete({
-             // Identify the entry to delete using phoneNumber
-            where: { phoneNumber }, 
-        });
-        // Send a 204 No Content status to indicate successful deletion
-        return res.status(204).end();
-    } catch (error) {
-        console.error('Error deleting demographic entry: ', error);
-        return res.status(400).json({ error: "Error deleting demographic entry." });
-    }
-}
-
-module.exports = {createDemographic, getDemographic, updateDemographic, deleteDemographic};
-*/
--- app/api/demographics/testDemographicsCrud.ts
+++ app/api/demographics/testDemographicsCrud.ts
@@ -1,27 +1,53 @@
 //testing file for Demographics CRUD functions
+//run npx prisma generate, npx prisma migrate dev & prisma db pull
+//run testing file using 'npx ts-node testDemographicsCrud.ts'
 
-//importing the crud functions
-//const crud = require("./route.ts");
-import { createDemographic } from './route';
+//importing the CRUD functions
+const CRUD = require("./route.ts");
 
 async function main(){
+    
     const testData = {
-        phoneNumber : '123456789',
+        phoneNumber : '123456788',
         takeCount: 2,
         donateCount: 5,
         name: "Viola Davis",
         householdSize: 4,
         address: "13 Winthrop Street",
-        lastVisitDate: Date
+        lastVisitDate: new Date("2023")
     };
+    
     try {
-        const testCreate = await createDemographic(testData);
-        console.log("created entry," testCreate);
+        //TEST CREATE
+        const testCreate = await CRUD.createDemographic(testData);
+        console.log("created entry", testCreate);
+        
+        //TEST READ
+        const testRead = await CRUD.getDemographic();
+        console.log("read entry", testRead);
+       
+        //TEST UPDATE
+        const testUpdateData = {
+            phoneNumber : '123456789',
+            takeCount: 5,
+            donateCount: 16,
+            name: "Emily Yuan",
+            householdSize: 2,
+            address: "28 Winthrop Street",
+            lastVisitDate: new Date("2024")
+        }
+        const testUpdate = await CRUD.updateDemographic(testUpdateData);
+        console.log("updated entry", testUpdate); 
+
+        //TEST DELETE
+        const testDelete = await CRUD.deleteDemographic('123456788');
+        console.log("deleted entry", testDelete);
+
     } catch (error) {
         console.error("error", error)
     }
-
 }
+
 
 main(); 
 
