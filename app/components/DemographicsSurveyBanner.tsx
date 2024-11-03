c7c7c61160b4a8743d2a8c416433d361c0e45e78
Amanda Wu <amandawu@Amandas-MacBook-Air.local>
2024-11-03 15:16:07 -0500

    made into tailwind structure

--- app/components/DemographicsSurveyBanner.tsx
+++ app/components/DemographicsSurveyBanner.tsx
@@ -1,47 +1,23 @@
 import React from 'react';
-import Image from 'next/image'
-import whiteOutlineLogo from '../images/headerLogo.png'
+import Image from 'next/image';
+import whiteOutlineLogo from '../images/headerLogo.png';
 
 export default function Banner() {
-    const myStyles: React.CSSProperties = {
-        position: 'absolute',
-        width: '100%',
-        height: '150px',
-        left: '0px',
-        top: '0px',
-        background: '#24593D',
-        alignItems: 'center',
-        justifyContent: 'center',
-        display: 'flex',
-   };
-
-    const imageStyles: React.CSSProperties = {
-        marginRight: '20px',
-        position: 'absolute',
-        left: '0px',
-        bottom: '0px',
-    };
-    
-   const textStyles: React.CSSProperties =  {
-      fontSize: 64,
-      textAlign: 'center',
-      fontFamily: "'Crimson Text', serif",
-      fontWeight: '700',
-    };
-    
     return ( 
-      <div style={myStyles} className="banner">
-        <div style={imageStyles}>
-            <Image
-                src={whiteOutlineLogo}
-                alt="logo"
-                width={196}
-                height={150}
-            />
+      <>
+        <div className="relative w-full h-[150px] bg-[#24593D] flex items-center justify-center">
+          <div className="absolute left-0 bottom-0 mr-5">
+              <Image
+                  src={whiteOutlineLogo}
+                  alt="logo"
+                  width={196}
+                  height={150}
+              />
+          </div>
+          <div className="text-center">
+            <h1 className="text-[64px] font-crimson font-bold text-white">Demographic Survey</h1>
+          </div>
         </div>
-        <div className="banner-content">
-          <h1 style={textStyles}>Demographic Survey</h1>
-        </div>
-      </div>
+      </>
     );
 }
