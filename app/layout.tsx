8879dc120eff8838564055824deac2471b74eef8
Jiyoon Choi <jiyoonchoi15@gmail.com>
2024-11-04 14:29:36 -0500

    moved font import to correct place

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,11 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <style>
-          {`@import url('https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&family=Quicksand&display=swap');`}
-        </style>
-      </head>
+      <head />
       <body>{children}</body>
     </html>
   );
