a24ec4c144982ef592803ce7a8d7c6af25381634
Charlie Koenig <charliek0303@gmail.com>
2024-11-11 16:31:37 -0500

    Added comments to help coding later

--- app/api/demographics/route.ts
+++ app/api/demographics/route.ts
@@ -3,6 +3,9 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient();
 
+// Charlie added for ticket 21
+// import { NextRequest, NextResponse } from 'next/server';
+
 //CREATE
 async function createDemographic(data: {
     phoneNumber : string,
@@ -75,3 +78,63 @@ export default {
     deleteDemographic
 };
 
+
+// charlie added for ticket 21
+// POST
+// export async function POST(req: NextRequest) {
+//     try {
+//         // get request body
+//         // const body = await req.json();
+        
+//         // check all fields were filled came through... is date made here??
+        
+//         // return result
+//     } catch (error) {
+//         // catch and return error
+//     }
+// }
+
+// GET
+// export async function GET() {
+//     try {
+//         // call get and return
+//     } catch (error) {
+//         // catch error
+//     }
+// }
+
+// PUT
+// export async function PUT(
+//     req: NextRequest,
+//     context: { params: { phoneNumber: string } }
+// ) {
+//     try {
+//         // make sure there is a phone number
+
+//         // do we require everything or just one field??
+//         // get update data from req body
+//         // const body = await req.json()
+        
+//         // do we update date on every PUT??
+
+//         // update
+//         // respond 200 on success
+//     } catch (error) {
+//         // catch error and responsd
+//     }
+// }
+
+// DELETE 
+// export async function DELETE(
+//     req: NextRequest,
+//     context: { params: { phoneNumber: string } }
+// ) {
+//     try {
+//         // make sure there was a phone number
+
+//         // delete based on number
+//         // respond 200 on success
+//     } catch (error) {
+//         // catch error and responsd
+//     }
+// }
