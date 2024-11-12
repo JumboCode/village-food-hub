efcd7817a2223323a0da537328aa4c08b4a5a673
danielrjakab1 <daniel.jakab@tufts.edu>
2024-11-11 21:16:29 -0500

    made changes

---
+++ app/components/Dropdowns.tsx
@@ -0,0 +1,9 @@
+//implements rounded dropdown menu
+export function NameDropdown() {
+    return (
+        <select className="select select-bordered w-full max-w-m -mt-10 rounded-xl">
+            <option disabled selected></option>
+            <option>Placeholder</option>
+        </select>
+    )
+}
--- app/volunteer-add-details/[[...volunteer-add-details]]/page.tsx
+++ app/volunteer-add-details/[[...volunteer-add-details]]/page.tsx
@@ -1,1 +1,57 @@
-// Write your code here
+import Banner from "../../components/UpdateInventoryBanner"
+import { ButtonBack, ButtonExit, ButtonNext } from "../../components/SurveyButtons"
+import { NameDropdown } from "../../components/Dropdowns";
+import React from 'react'
+
+
+// This function returns the page that allows Volunteers to input data on the
+// contents they are removing from the inventory
+const VolunteerRemoveDetails: React.FC = () => {
+    return (
+        <div>
+            <Banner/>
+            {/* Back and Exit buttons */}
+            <div className = "flex flex-row h-full w-full justify-between mt-10 px-40 py-18">
+                <div className= "flex flex-3">
+                    <ButtonBack/>
+                </div>
+                <ButtonExit/>
+            </div>
+            {/* Form Contents */}
+            <div className = "flex flex-col h-1/2 w-3/5 justify-center font-crimson justify-self-center mt-5">
+                <p className="justify-self-center text-[48px] font-bold">What are you adding?</p>
+                <div className = "font-bold text-[36px]">
+                    {/* category name dropdown */}
+                    <p>Category Name  </p>
+                    <NameDropdown/>
+                </div>
+                <div className = "font-bold text-[36px]">
+                    {/* item name dropdown */}
+                    <p>Item Name  </p>
+                    <NameDropdown/>
+                </div>
+                <div className = "flex flex-row w-full justify-between">
+                <div className = "font-bold text-[36px] pt-10 -p-10">
+                    {/* textbox for quantity */}
+                        <p>Quantity</p>
+                        <input
+                            type="text"
+                            placeholder=""
+                            className="input input-bordered input-xs w-full max-w-xs rounded-xl" />
+                    </div>
+                    {/* dropdown for amount of units */}
+                    <div className = "font-bold text-[36px] pt-10 -p-10">
+                        <p>Units</p>
+                        <NameDropdown/>
+                    </div>
+                </div>
+                {/* Next button */}
+                <div className = " flex justify-center mt-10">
+                    <ButtonNext/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default VolunteerRemoveDetails;
