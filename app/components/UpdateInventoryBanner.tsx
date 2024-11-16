e2bc2201104b7005d4f55eade3b2f87ca7bdc59f
Jiyoon Choi <jiyoonchoi15@gmail.com>
2024-11-15 20:57:37 -0500

    made the banner smaller

--- app/components/UpdateInventoryBanner.tsx
+++ app/components/UpdateInventoryBanner.tsx
@@ -5,17 +5,17 @@ import whiteOutlineLogo from '../images/headerLogo.png';
 export default function UpdateInventoryBanner() {
     return ( 
       <>
-        <div className="relative w-full h-[150px] bg-banner-green flex items-center justify-center shadow-2xl">
+        <div className="relative w-full h-[120px] bg-banner-green flex items-center justify-center shadow-xl">
           <div className="absolute left-0 bottom-0 mr-5">
               <Image
                   src={whiteOutlineLogo}
                   alt="logo"
-                  width={196}
-                  height={150}
+                  width={160}
+                  height={130}
               />
           </div>
           <div className="text-center">
-            <h1 className="text-[64px] font-crimson font-bold text-white">Update Inventory</h1>
+            <h1 className="text-[54px] font-crimson font-bold text-white">Update Inventory</h1>
           </div>
         </div>
       </>
