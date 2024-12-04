242162b4137eed24d82410573e33b65b6eaf7a38
alexanderviolet <alexander.violet@tufts.edu>
2024-12-03 20:24:42 -0500

    almost done with customer-donor

--- app/components/SurveyButtons.tsx
+++ app/components/SurveyButtons.tsx
@@ -133,3 +133,31 @@ export function ButtonEdit({ onClick, disabled }: ButtonProps) {
       </div>
   );
 }
+
+export function YesProceed ({ onClick, disabled }: ButtonProps) {
+    return (
+        <div>
+            <button 
+            className="w-[300px] bg-light-green hover:bg-dark-green text-white font-serif py-2 px-8 rounded-full"
+            onClick={onClick}
+              disabled={disabled}
+            >
+                { "Yes, proceed to survey" }
+            </button>
+        </div>
+    );
+  }
+
+export function NoDone ({ onClick, disabled }: ButtonProps) {
+    return (
+        <div>
+            <button 
+            className="w-[300px] bg-red hover:bg-dark-red text-white font-serif py-2 px-8 rounded-full"
+            onClick={onClick}
+              disabled={disabled}
+            >
+                { "No, I'm done" }
+            </button>
+        </div>
+    );
+  }
