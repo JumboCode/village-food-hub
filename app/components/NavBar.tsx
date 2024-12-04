083500f631cd775f6636166fb50caa3955a7f2a6
Emily Yuan <emyuans@yahoo.com>
2024-12-03 21:19:07 -0500

    loaded page for NavBar

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Image from 'next/image';
+import whiteOutlineLogo from '@app/images/headerLogo.png';
+
+export default function NavBar() {
+    return ( 
+      <>
+        <div className="relative w-full h-[120px] bg-banner-green flex items-center justify-center shadow-xl">
+          <div className="absolute left-0 bottom-0 mr-5">
+              <Image
+                  src={whiteOutlineLogo}
+                  alt="logo"
+                  width={160}
+                  height={130}
+              />
+          </div>
+          {/* <div className="text-center"> */}
+            <h1 className="text-left text-[24px] font-crimson font-bold text-white">Demographics</h1>
+          {/* </div> */}
+        </div>
+      </>
+    );
+}
