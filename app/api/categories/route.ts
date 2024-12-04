f15c969cdef6113c3b5bcc5c18176acfe0bc7fdc
Sofia A <alvazzi.sofia@gmail.com>
2024-12-03 21:16:45 -0500

    first commit

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,3 +1,4 @@
+import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
@@ -57,6 +58,138 @@ async function deleteCategory(data: {
         }
         });
     }
+    
+// POST
+export async function POST(req: NextRequest) {
+    try {
+        const record = await req.json()
+        if (!validCategory(record)) {
+            return NextResponse.json(
+                { response : "Invalid data format" }, 
+                { status : 400 }
+            )
+        }
+        console.log("works right before")
+        const response = await createCategory({ ...record })
+        console.log("second to last")
+        return NextResponse.json(response, {status : 201})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json(
+            { response : "Failed to create record" }, 
+            { status : 500 })
+    }
+}
+
+// GET
+export async function GET() {
+    try {
+        const items = await readCategories()
+        return NextResponse.json(items, { status : 200 })
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json(
+            { response : "Failed to get categories" }, 
+            { status : 500 }
+        )
+    }
+}
+
+// PUT
+export async function PUT(
+    req: NextRequest,
+) {    
+    try {
+        const data = await req.json()
+        if (!("itemName" in data)) {
+            return NextResponse.json( 
+                { response: "Missing item name" },
+                { status: 400 }
+            )
+        }
+        if (!("name" in data)) {
+            return NextResponse.json( 
+                { response: "Missing name" },
+                { status: 400 }
+            )
+        }
+        const item = await updateCategory({ data.itemName });
+        
+        return NextResponse.json(item, { status : 200 })
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json(
+            { response : "Failed to update entry" },
+            { status : 500 }
+        )
+    }
+}
+
+
+// DELETE 
+export async function DELETE(
+    req: NextRequest
+) {
+    try {
+        const data = await req.json()
+        if (!("itemName" in data)) {
+            return NextResponse.json( 
+                { response: "Missing item name" },
+                { status: 400 }
+            )
+        }
+        if (!("name" in data)) {
+            return NextResponse.json( 
+                { response: "Missing name" },
+                { status: 400 }
+            )
+        }
+        
+        const item = await deleteCategory({
+            itemName: data.itemName, 
+            name: data.name 
+        })
+        return NextResponse.json(item, {status : 200})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json( 
+            { response : "Failed to delete record" },
+            { status : 500 }
+        )
+    }
+}
+
+interface CategoryRecord {
+    itemName: String;
+    units:    String[];
+    name:     String;
+  }
+
+function validCategory(record : CategoryRecord): boolean {
+
+    try {
+        const fields = new Set<string>([
+            "itemName",
+            "units",
+            "name",
+          ]);
+        
+        const keys = Object.keys(record)
+        if (keys.length !== fields.size) return false
+
+        let fieldsMatch = true
+        keys.forEach( (field: string) => {
+            if (!fields.has(field)) fieldsMatch = false
+            fields.delete(field)
+        })
+        
+        return fieldsMatch
+        
+    } catch (error) {
+        console.log(error)
+        return false
+    }
+}
 
 module.exports = {createCategory, readCategories, updateCategory, deleteCategory};
         
