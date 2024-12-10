fbea0a374539a90ac6f8bdb69406588d87d9e9e7
Emily Yuan <emyuans@yahoo.com>
2024-12-09 20:10:43 -0500

    Finished last parts of NavBar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import Image from 'next/image';
 import whiteOutlineLogo from '@app/images/headerLogo.png';
@@ -8,9 +9,20 @@ import initials from '@app/images/group2.png';
 import face from '@app/images/Frame6.png';
 import settings from '@app/images/Frame7.png';
 import icon from '@app/images/Frame8.png';
+import downArrow2 from '@app/images/downArrow.png';
 
 export default function NavBar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const router = useRouter(); 
+
+  const handleSignOut = () => {
+    router.push("/login/page");
+  };
+
+  const handleInventory = () => {
+    router.push("/inventory/page");
+  };
+  
     return (
         <>
             <div className="relative w-full h-[120px] bg-banner-green flex items-center shadow-xl">
@@ -26,13 +38,17 @@ export default function NavBar() {
 
                 {/* Navigation Links */}
                 <div className="flex space-x-40">
-                    <button className="text-[24px] font-crimson font-bold text-white px-6 py-3 rounded-xl bg-transparent hover:bg-[#D9D9D9]">
+                    <button className="text-[24px] font-crimson font-bold 
+                    text-white px-6 py-3 rounded-xl bg-transparent hover:bg-[#D9D9D9]">
                         Demographics
                     </button>
-                    <button className="text-[24px] font-crimson font-bold text-white px-6 py-3 rounded-xl bg-transparent hover:bg-[#D9D9D9]">
+                    <button className="text-[24px] font-crimson font-bold 
+                    text-white px-6 py-3 rounded-xl bg-transparent hover:bg-[#D9D9D9]"
+                    onClick={handleInventory}>
                         Inventory
                     </button>
-                    <button className="text-[24px] font-crimson font-bold text-white px-6 py-3 rounded-xl bg-transparent hover:bg-[#D9D9D9]">
+                    <button className="text-[24px] font-crimson font-bold 
+                    text-white px-6 py-3 rounded-xl bg-transparent hover:bg-[#D9D9D9]">
                         Categories
                     </button>
                 </div>
@@ -55,10 +71,10 @@ export default function NavBar() {
                       className="flex items-center text-[24px] font-crimson font-bold text-white">
                       Glen McLeod
                       <Image
-                          src={dropArrow}
+                          src={isDropdownOpen ? downArrow2 : dropArrow}
                           alt="drop-down-arrow"
-                          width={8.59}
-                          height={14.85}
+                          width={isDropdownOpen ? 25 : 8.59}
+                          height={isDropdownOpen ? 30 : 14.85}
                           className="ml-2"
                       />
                   </button>
@@ -66,15 +82,16 @@ export default function NavBar() {
                 {isDropdownOpen && (
                         <div className="absolute right-0 mt-2 bg-white rounded-xl shadow-lg w-48 z-50">
                             <ul>
-                            <li className="flex items-center text-[24px] font-crimson font-bold px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                            <li className="flex items-center text-[24px] font-crimson font-bold px-4 py-2 hover:bg-[#ECF9E9] cursor-pointer">
                             <Image src={face} alt="logo" width={24} height={24} className="mr-2" />
                               My Profile
                             </li>
-                            <li className="flex items-center text-[24px] font-crimson font-bold px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                            <li className="flex items-center text-[24px] font-crimson font-bold px-4 py-2 hover:bg-[#ECF9E9] cursor-pointer">
                             <Image src={settings} alt="settings-logo" width={24} height={24} className="mr-2" />
                               Settings
                             </li>
-                            <li className="flex items-center text-[24px] font-crimson font-bold px-4 py-2 hover:bg-gray-200 cursor-pointer">
+                            <li className="flex items-center text-[24px] font-crimson font-bold px-4 py-2 hover:bg-[#ECF9E9] cursor-pointer"
+                            onClick={handleSignOut}>
                             <Image src={icon} alt="icon" width={24} height={24} className="mr-2" />
                               Sign Out
                             </li>
--- app/demographics/[[...demographics]]/page.tsx
+++ app/demographics/[[...demographics]]/page.tsx
@@ -3,10 +3,6 @@ import NavBar from '@app/components/NavBar';
 export default function DemographicsPage(){
     return (
         <NavBar 
-            // currentTab="demographics"
-            // firstName="Glen"
-            // lastName="McLeod"
-            // pictureLink="/group2.png"
         />
     );
 }
