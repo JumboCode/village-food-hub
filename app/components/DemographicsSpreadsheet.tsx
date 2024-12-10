e7f3f48dfb6942f1c08f1835d21266b74c1646c1
Emily Yuan <emyuans@yahoo.com>
2024-12-09 20:49:05 -0500

    started demographics spreadsheet

---
+++ app/components/DemographicsSpreadsheet.tsx
@@ -0,0 +1,134 @@
+import React from "react";
+import Image from 'next/image';
+import deleteIcon from '../images/delete.png';
+import downloadIcon from "../images/download.png";
+import editIcon from "../images/edit.png";
+import arrowsIcon from "../images/upAndDownArrows.png";
+
+interface DemographicsSpreadsheetProps {
+    demographicsItems: (string | number)[][];
+}
+
+export const DemographicsSpreadsheet: React.FC<DemographicsSpreadsheetProps> = ({ demographicsItems = [] }) => {
+    console.log("demographicsItems:", demographicsItems);
+
+    return(
+        <div className="relative overflow-x-auto crimson-regular font-crimson">
+        <table className="table-auto w-full">
+            <thead className ="font-crimson border- crimson-regular border-separate content-start">
+                <tr className="bg-dark-blue text-white text-lg align-left ">
+                <th className="border-collapse border-zinc-50 border-2 border-y-1 py-2 px-3">
+                    <div className="flex flex-row justify-between">
+                        <p>Date</p>
+                        <Image src={arrowsIcon}
+                                    width={15}
+                                    height={15}
+                                    alt="arrows Icon"
+                                    className="">
+                                    </Image>
+                        </div>
+                    </th>
+                <th className="border-collapse border-zinc-50 border-2 border-y-1 py-2 px-3">
+                <div className="flex flex-row justify-between">
+                        <p>Phone Number</p>
+                        </div>
+                </th>
+                <th className="border-collapse border-zinc-50 border-2 border-y-1 py-2 px-3">
+                <div className="flex flex-row justify-between">
+                        <p>Name</p>
+                        <Image src={arrowsIcon}
+                                    width={15}
+                                    height={15}
+                                    alt="arrows Icon"
+                                    className="">
+                                    </Image>
+                        </div>
+                </th>
+                <th className="border-collapse border-zinc-50 border-2 border-y-1 py-2 px-3">
+                <div className="flex flex-row justify-between">
+                        <p>Address</p>
+                        </div>
+                </th>
+                <th className="border-collapse border-zinc-50 border-2 border-y-1 py-2 px-3">
+                    <div className="flex flex-row justify-between">
+                        <p>House Size</p>
+                        <Image src={arrowsIcon}
+                                    width={15}
+                                    height={15}
+                                    alt="arrows Icon"
+                                    className="">
+                                    </Image>
+                        </div>
+                    </th>
+                    <th className="border-collapse border-zinc-50 border-2 border-y-1 py-2 px-3">
+                    <div className="flex flex-row justify-between">
+                        <p>Received</p>
+                        <Image src={arrowsIcon}
+                                    width={15}
+                                    height={15}
+                                    alt="arrows Icon"
+                                    className="">
+                                    </Image>
+                        </div>
+                    </th>
+                    <th className="border-collapse border-zinc-50 border-2 border-y-1 py-2 px-3">
+                    <div className="flex flex-row justify-between">
+                        <p>Donated</p>
+                        <Image src={arrowsIcon}
+                                    width={15}
+                                    height={15}
+                                    alt="arrows Icon"
+                                    className="">
+                                    </Image>
+                        </div>
+                    </th>
+                <th className="border-collapse border-zinc-50 border-2 border-y-1 py-2 px-3">Units</th>
+                <th className="border-collapse border-zinc-50 border-2 border-y-1 py-2 px-3">Actions</th>
+                </tr>
+            </thead>
+            <tbody className="bg-zinc-75 border-collapse border-zinc-400 font-crimson crimson-regular">
+            {demographicsItems.map((item, index) => (
+                        <tr key={index} className="py-2">
+                            {item.map((data, subIndex) => (
+                                <td
+                                    key={subIndex}
+                                    className="border-collapse border-zinc-200 border-2 border-y-1 py-2 px-3"
+                                >
+                                    {data}
+                                </td>
+                            ))}
+                            <td
+                                key={`actions-${index}`}
+                                className="flex row justify-around border-collapse border-zinc-300 border-2 border-y-1 py-2 px-3"
+                            >
+                                <Image
+                                    src={editIcon}
+                                    width={18}
+                                    height={18}
+                                    alt="edit Icon"
+                                    className=""
+                                />
+                                <Image
+                                    src={deleteIcon}
+                                    width={18}
+                                    height={18}
+                                    alt="delete Icon"
+                                    className=""
+                                />
+                                <Image
+                                    src={downloadIcon}
+                                    width={18}
+                                    height={18}
+                                    alt="download Icon"
+                                    className=""
+                                />
+                            </td>
+                        </tr>
+                    ))}
+            </tbody>
+            </table>
+        </div>
+
+    )
+
+}
--- app/demographics/[[...demographics]]/page.tsx
+++ app/demographics/[[...demographics]]/page.tsx
@@ -1,8 +1,27 @@
-import NavBar from '@app/components/NavBar';
+import React from "react";
+import NavBar from "@app/components/NavBar";
+import { DemographicsSpreadsheet } from "@app/components/DemographicsSpreadsheet";
 
-export default function DemographicsPage(){
+const InternalViewDemographicsPage: React.FC = () => {
+    const demographicsItems = [
+        ["2024-01-01", "415273832", "Tanisha", 10, "pcs"],
+        ["2024-01-02", "344343343", "Emily", 20, "kg"],
+        ["2024-01-03", "234382392", "Karen", 15, "pcs"],
+    ];
+
     return (
-        <NavBar 
-        />
+        <div className="p-4">
+            <h1 className="text-3xl font-bold mb-4">Demographic Responses</h1>
+            <DemographicsSpreadsheet demographicsItems={demographicsItems} />
+        </div>
+    );
+};
+
+export default function DemographicsPage() {
+    return (
+        <>
+            <NavBar />
+            <InternalViewDemographicsPage />
+        </>
     );
 }
