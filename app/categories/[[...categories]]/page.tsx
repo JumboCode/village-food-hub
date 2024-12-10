49f11588d6ded8484adc0e3a2f041cff60da1fb3
zoyaamrit <tanvi.amrit@tufts.edu>
2024-12-10 14:28:50 -0500

    Fix button alignment and add dropdown icons

--- app/categories/[[...categories]]/page.tsx
+++ app/categories/[[...categories]]/page.tsx
@@ -1,8 +1,12 @@
 'use client';
 
 import React, { useState } from 'react';
+import Image from 'next/image';
+
 import { CategoriesSpreadsheet } from '@app/components/CategoriesSpreadsheet';
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faPlus} from '@fortawesome/free-solid-svg-icons'
+import deleteIcon from "../../images/delete.png"
+import editIcon from "../../images/edit.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { NameDropdown } from '@app/components/Dropdowns';
 import { InventorySpreadsheet } from '@app/components/InventorySpreadsheet';
@@ -31,21 +35,42 @@ const Categories: React.FC<{ onChange: (value: string) => void }> = ({ onChange 
             <div className='flex justify-center items-center'> 
                 <div className="w-3/5 h-4/5 ">
                     <div className='flex flex-col'>
-                    <p className="crimson-bold text-[24px] pt-5 ">Edit Category</p>
+                        <p className="crimson-bold text-[24px] pt-5 ">Edit Category</p>
                         <div className='flex justify-between items-center py-4 w-full'>
-                        <div className='flex flex-col w-1/2'>
-                            <div className="pt-">
+                        <div className='flex flex-row items-center w-1/2'>
+                            <div className='w-full'>
                                 <NameDropdown options={optionsArr} onSelect={switchState} />
                             </div>
+                            {showTable && (
+                                <Image
+                                src={deleteIcon}
+                                width={18}
+                                height={18}
+                                alt="delete Icon"
+                                className="m-2"
+                            />
+                            )}
+                            {showTable && (
+                                <Image
+                                src={editIcon}
+                                width={18}
+                                height={18}
+                                alt="delete Icon"
+                                className="m-2"
+                            />
+                            )}
                         </div>
 
-                        {showTable && (<button className="bg-light-green hover:bg-dark-green text-white font-serif py-[5px] px-[20px] rounded text-[20px]">
-                            { "Item "} <FontAwesomeIcon className='' icon={faPlus} style={{ fontSize: '14px' }}/>
-                        </button>)}
-                    
-                        <button className="bg-light-green hover:bg-dark-green text-white font-serif py-[5px] px-[20px] rounded text-[20px]">
-                            { "Category "} <FontAwesomeIcon className='' icon={faPlus} style={{ fontSize: '14px' }}/>
-                        </button>
+                        <div>
+
+                            {showTable && (<button className="bg-light-green hover:bg-dark-green text-white font-serif pt-1 pb-1 px-1 mr-2 mb-2 rounded text-[20px]">
+                                { "Item "} <FontAwesomeIcon className='' icon={faPlus} style={{ fontSize: '14px' }}/>
+                            </button>)}
+                        
+                            <button className="bg-light-green hover:bg-dark-green text-white font-serif pt-1 pb-1 px-1 mb-2 rounded text-[20px]">
+                                { "Category "} <FontAwesomeIcon className='' icon={faPlus} style={{ fontSize: '14px' }}/>
+                            </button>
+                        </div>
 
 
                     </div>
@@ -64,3 +89,6 @@ const Categories: React.FC<{ onChange: (value: string) => void }> = ({ onChange 
 
 export default Categories; 
 
+
+/*
+pt-1 pb-1 px-1 mb-2 */
