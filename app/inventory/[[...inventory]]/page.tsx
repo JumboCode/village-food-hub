60bc0ab677d56e035590a5d13d55a4a63e42eb06
Jiyoon Choi <jiyoonchoi15@gmail.com>
2025-01-13 13:23:32 -0500

    made some small fixes to the inventory spreadsheet

--- app/inventory/[[...inventory]]/page.tsx
+++ app/inventory/[[...inventory]]/page.tsx
@@ -1,42 +1,61 @@
 "use client"
-import React, { useEffect }from "react";
+import React, { useEffect } from "react";
 import { InventorySpreadsheet } from '@app/components/InventorySpreadsheet';
+
+// Utility function to format date to dd/mm/yyyy
+function formatDate(date: Date): string {
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const year = date.getFullYear();
+  return `${month}/${day}/${year}`;
+}
+
 function getInventory() {
   try {
-    return fetch("/../api/inventory", {method : 'GET'})
-    .then((response) => {
+    return fetch("/../api/inventory", { method: 'GET' })
+      .then((response) => {
         if (!response.ok) throw response;
         return response.json();
-    })
-    .then ((jsonData : any) => jsonData.data )
-    .then((inventoryObjects : any) => {
-      const listOfLists = inventoryObjects.map((object : any) => {
-        let fields = Object.values(object);
-        var date = new Date(fields[fields.length - 1] as string);
-        var datestring =  (date.getMonth()+1) + "-" + date.getDate() + "-" + date.getFullYear();
-        fields[fields.length - 1] = datestring;
-        console.log(Object.values(object))
-        return fields;
+      })
+      .then((jsonData: any) => jsonData.data)
+      .then((inventoryObjects: any) => {
+        const listOfLists = inventoryObjects.map((object: any) => {
+          let fields = Object.values(object);
+          // Remove the last field (history object) because it conflicts with the spreadsheet
+          fields.pop();
+
+          // Format the date field if it exists
+          const dateFieldIndex = fields.length - 1;
+          const dateField = fields[dateFieldIndex];
+          const date = new Date(dateField as string);
+
+          if (!isNaN(date.getTime())) {
+            fields[dateFieldIndex] = formatDate(date);
+          } else {
+            fields[dateFieldIndex] = "";
+          }
+
+          return fields;
+        });
+
+        console.log("List of Lists:", listOfLists);
+        return listOfLists;
       });
-      
-      console.log("List of Lists:", listOfLists)
-      return listOfLists
-    })
-  } catch(error) {
+  } catch (error) {
     console.error(error);
     return Promise.resolve([]);
   }
-  
 }
+
 const InternalViewInventoryPage: React.FC = () => {
   const [inventory, setInventory] = React.useState<any>();
   useEffect(() => {
     getInventory()
-      .then((items: any) => {setInventory(items)})
-   }, []);
+      .then((items: any) => { setInventory(items) })
+  }, []);
   return (
     <InventorySpreadsheet inventoryItems={inventory} />
   );
 };
- 
+
 export default InternalViewInventoryPage;
