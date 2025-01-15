c340460e42c112310c6f5a6efa905d3157285e51
Jiyoon Choi <jiyoonchoi15@gmail.com>
2025-01-15 13:51:29 -0500

    tweaked styling

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import Image from 'next/image';
@@ -16,91 +16,112 @@ export default function NavBar() {
   const router = useRouter(); 
 
   const handleSignOut = () => {
-    router.push("/login/page");
+    router.push("/login");
+  };
+
+  const handleDemographics = () => {
+    router.push("/demographics");
   };
 
   const handleInventory = () => {
-    router.push("/inventory/page");
+    router.push("/inventory");
   };
-  
-    return (
-        <>
-            <div className="relative w-full h-[120px] bg-banner-green flex items-center shadow-xl">
-                {/* Logo Section */}
-                <div className="flex-shrink-0 mr-8">
-                    <Image
-                        src={whiteOutlineLogo}
-                        alt="logo"
-                        width={160}
-                        height={130}
-                    />
-                </div>
 
-                {/* Navigation Links */}
-                <div className="flex space-x-40">
-                    <button className="text-[24px] font-crimson font-bold 
-                    text-white px-6 py-3 rounded-xl bg-transparent hover:bg-[#D9D9D9]">
-                        Demographics
-                    </button>
-                    <button className="text-[24px] font-crimson font-bold 
-                    text-white px-6 py-3 rounded-xl bg-transparent hover:bg-[#D9D9D9]"
-                    onClick={handleInventory}>
-                        Inventory
-                    </button>
-                    <button className="text-[24px] font-crimson font-bold 
-                    text-white px-6 py-3 rounded-xl bg-transparent hover:bg-[#D9D9D9]">
-                        Categories
-                    </button>
-                </div>
+  const handleCategories = () => {
+    router.push("/categories");
+  };
 
-                <div className="absolute right-10 flex items-center space-x-4">
-                
-                {/* User Initials */}
-                  <Image
-                    src={initials}
-                    alt="initial_letters"
-                    width={51}
-                    height={51}
-                    className="rounded-full"
-                  />
+  const [currentPath, setCurrentPath] = useState("");
 
-                  {/* User Name and Dropdown */}
-                <div className="relative">
-                    <button
-                        onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-                      className="flex items-center text-[24px] font-crimson font-bold text-white">
-                      Glen McLeod
-                      <Image
-                          src={isDropdownOpen ? downArrow2 : dropArrow}
-                          alt="drop-down-arrow"
-                          width={isDropdownOpen ? 25 : 8.59}
-                          height={isDropdownOpen ? 30 : 14.85}
-                          className="ml-2"
-                      />
-                  </button>
-                {/* Dropdown Menu */}
-                {isDropdownOpen && (
-                        <div className="absolute right-0 mt-2 bg-white rounded-xl shadow-lg w-48 z-50">
-                            <ul>
-                            <li className="flex items-center text-[24px] font-crimson font-bold px-4 py-2 hover:bg-[#ECF9E9] cursor-pointer">
-                            <Image src={face} alt="logo" width={24} height={24} className="mr-2" />
-                              My Profile
-                            </li>
-                            <li className="flex items-center text-[24px] font-crimson font-bold px-4 py-2 hover:bg-[#ECF9E9] cursor-pointer">
-                            <Image src={settings} alt="settings-logo" width={24} height={24} className="mr-2" />
-                              Settings
-                            </li>
-                            <li className="flex items-center text-[24px] font-crimson font-bold px-4 py-2 hover:bg-[#ECF9E9] cursor-pointer"
-                            onClick={handleSignOut}>
-                            <Image src={icon} alt="icon" width={24} height={24} className="mr-2" />
-                              Sign Out
-                            </li>
-                            </ul>
-                        </div>
-                    )}
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
+
+  return (
+    <>
+      <div className="relative w-full h-[90px] bg-banner-green flex items-center shadow-xl">
+        {/* Logo Section */}
+        <div className="flex-shrink-0 mr-8">
+          <Image
+            src={whiteOutlineLogo}
+            alt="logo"
+            width={112}
+            height={91}
+          />
+        </div>
+
+        {/* Navigation Links */}
+        <div className="flex space-x-10">
+          <button
+            className={`text-[21px] font-crimson font-bold text-white px-6 py-3 rounded-xl ${currentPath === '/demographics' ? 'bg-[#D9D9D9] hover:bg-opacity-90 bg-opacity-30' : 'bg-transparent hover:bg-[#D9D9D9] hover:bg-opacity-30'}`}
+            onClick={handleDemographics}
+          >
+            Demographics
+          </button>
+          <button
+            className={`text-[21px] font-crimson font-bold text-white px-6 py-3 rounded-xl ${currentPath === '/inventory' ? 'bg-[#D9D9D9] hover:bg-opacity-90 bg-opacity-30' : 'bg-transparent hover:bg-[#D9D9D9] hover:bg-opacity-30'}`}
+            onClick={handleInventory}
+          >
+            Inventory
+          </button>
+          <button
+            className={`text-[21px] font-crimson font-bold text-white px-6 py-3 rounded-xl ${currentPath === '/categories' ? 'bg-[#D9D9D9] hover:bg-opacity-90 bg-opacity-30' : 'bg-transparent hover:bg-[#D9D9D9] hover:bg-opacity-30'}`}
+            onClick={handleCategories}
+          >
+            Categories
+          </button>
+        </div>
+
+        <div className="absolute right-10 flex items-center space-x-4">
+          {/* User Initials */}
+          <Image
+            src={initials}
+            alt="initial_letters"
+            width={51}
+            height={51}
+            className="rounded-full"
+          />
+
+          {/* User Name and Dropdown */}
+          <div className="relative">
+            <button
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+              className="flex items-center text-[21px] font-crimson font-bold text-white"
+            >
+              Glen McLeod
+              <Image
+                src={isDropdownOpen ? downArrow2 : dropArrow}
+                alt="drop-down-arrow"
+                width={isDropdownOpen ? 25 : 8.59}
+                height={isDropdownOpen ? 30 : 14.85}
+                className="ml-2"
+              />
+            </button>
+            {/* Dropdown Menu */}
+            {isDropdownOpen && (
+              <div className="absolute right-0 mt-7 bg-white rounded-md shadow-lg w-48 z-50">
+                <ul>
+                  <li className="flex items-center text-[21px] rounded-md font-crimson font-bold px-4 py-2 hover:bg-[#ECF9E9] cursor-pointer">
+                    <Image src={face} alt="logo" width={24} height={24} className="mr-2" />
+                    My Profile
+                  </li>
+                  <li className="flex items-center text-[21px] font-crimson font-bold px-4 py-2 hover:bg-[#ECF9E9] cursor-pointer">
+                    <Image src={settings} alt="settings-logo" width={24} height={24} className="mr-2" />
+                    Settings
+                  </li>
+                  <li
+                    className="flex items-center text-[21px] rounded-md font-crimson font-bold px-4 py-2 hover:bg-[#ECF9E9] cursor-pointer"
+                    onClick={handleSignOut}
+                  >
+                    <Image src={icon} alt="icon" width={24} height={24} className="mr-2" />
+                    Sign Out
+                  </li>
+                </ul>
               </div>
-            </div>
+            )}
           </div>
-        </>
-    );
+        </div>
+      </div>
+    </>
+  );
 }
