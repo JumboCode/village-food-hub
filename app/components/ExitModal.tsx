5bce5a55824351be15a3243193d66fd4a8203c80
Jiyoon Choi <jiyoonchoi15@gmail.com>
2025-01-26 16:29:05 -0500

    added z-50 to modal which forces the modal to be above/overlaying everything

--- app/components/ExitModal.tsx
+++ app/components/ExitModal.tsx
@@ -17,7 +17,7 @@ const ExitModal: React.FC<ExitModalProps> = ({ closeModal, redirectPage }) => {
   };
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div
         className="h-[240px] w-[550px] bg-modal-gray font-crimson
                    fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
