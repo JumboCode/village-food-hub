e45c8c23b7f3a35b051c33e9baaab1cbaa7e8a57
alexanderviolet <alexander.violet@tufts.edu>
2025-02-27 20:11:18 -0500

    Frontend, but TODO backend

--- app/categories/[[...categories]]/page.tsx
+++ app/categories/[[...categories]]/page.tsx
@@ -22,8 +22,9 @@ const Categories: React.FC = () => {
     const [selectedCategory, setSelectedCategory] = useState<string>('');
     const [showTable, setShowTable] = useState(false);
     const [showCategoryModal, setShowCategoryModal] = useState(false);
+    const [showEditModal, setShowEditModal] = useState(false);
     const [showItemModal, setShowItemModal] = useState(false);
-    const [selectedItem, setSelectedItem] = useState<string>('');
+    const [selectedItem, setSelectedItem] = useState<string>(''); // This has build error not used
     const [categoryName, setCategoryName] = useState("");
     const [itemName, setItemName] = useState("");
     const [showEmptyError, setShowEmptyError] = useState(false);
@@ -73,13 +74,18 @@ const Categories: React.FC = () => {
         setShowCategoryModal(false);
         setShowEmptyError(false);
         setRetrievalError(false);
-
+        setShowEditModal(false);
     };
 
     // Open modal to add a new item
     const itemButtonClicked = () => {
         setShowItemModal(true);
     };
+
+    const editButtonClicked = () => {
+        setShowEditModal(true);
+        console.log("edit button clicked function!")
+    }
 
     // Close modal and reset error states
     const itemModalClosed = () => {
@@ -189,13 +195,15 @@ const Categories: React.FC = () => {
                                             alt="delete Icon"
                                             className="m-4 ml-6 mt-2"
                                         />
+                                        <button onClick={editButtonClicked}>
                                         <Image
                                             src={editIcon}
                                             width={18}
                                             height={18}
                                             alt="edit Icon"
                                             className="m-2 mb-3.5"
-                                        />
+                                            />
+                                        </button>
                                     </>
                                 )}
                             </div>
@@ -307,6 +315,39 @@ const Categories: React.FC = () => {
                             <button
                                 className="flex bg-light-green hover:bg-dark-green text-white font-serif w-[117px] height-[46px] rounded-[8px] border-[1px] border-gray text-[20px] justify-center"
                                 onClick={saveButtonClicked}
+                            >
+                                Save
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+            {showEditModal && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <div className="h-[230px] w-[412px] bg-[#FFFFFF] font-crimson justify-center items-center py-[20px] shadow-lg rounded-[7px] border-[2px] border-light-green">
+                        <p className="text-center text-[32px] font-bold pb-[15px]">Edit Name</p>
+                        <div className="flex w-full justify-center items-center pb-[30px]">
+                            <input
+                                type="text"
+                                // onChange={(e) => setCategoryName(e.target.value)}
+                                className="flex w-[242px] h-[50px] bg-inherit rounded-[13px] border-[3px] border-[#E1E1E1] justify-center"
+                            />
+                        </div>
+                        {showEmptyError && (
+                            <p className="absolute w-[412px] text-center top-1/2 pt-5 text-red">
+                                Please enter a category name.
+                            </p>
+                        )}
+                        <div className="flex w-full justify-center space-x-[15px] items-center">
+                            <button
+                                className="flex text-gray hover:bg-white font-serif w-[117px] height-[46px] rounded-[8px] border-[1px] border-gray text-[20px] justify-center"
+                                onClick={cancelButtonClicked}
+                            >
+                                Cancel
+                            </button>
+                            <button
+                                className="flex bg-light-green hover:bg-dark-green text-white font-serif w-[117px] height-[46px] rounded-[8px] border-[1px] border-gray text-[20px] justify-center"
+                                // onClick={saveButtonClicked}
                             >
                                 Save
                             </button>
