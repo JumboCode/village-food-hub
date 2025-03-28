36f54b79045dabf8f77aab6499bebca3fbd37200
Jiyoon Choi <jiyoonchoi15@gmail.com>
2025-03-28 17:15:01 -0400

    added code that prepopulates quantity modal input with current quantity

--- app/components/InventorySpreadsheet.tsx
+++ app/components/InventorySpreadsheet.tsx
@@ -38,6 +38,7 @@ export const InventorySpreadsheet: React.FC<InventorySpreadsheetProps> = ({ inve
     const [itemName, setItemName] = useState<string | null>(null);
     const [units, setUnits] = useState<string | null>(null);
     const [currCategoryName, setCurrCategoryName] = useState<string | null>(null);
+    const [currentQuantity, setCurrentQuantity] = useState<number>(0);
 
     const closeQuantityModal = (): void => {
         setShowQuantityModal(false);
@@ -46,11 +47,12 @@ export const InventorySpreadsheet: React.FC<InventorySpreadsheetProps> = ({ inve
         setCurrCategoryName(null);
     };
 
-    const openQuantityModal = (itemName: string, units: string, category: string): void => {
-        setShowQuantityModal(true);
-        setItemName(itemName);
-        setUnits(units);
-        setCurrCategoryName(category);
+    const openQuantityModal = (itemName: string, units: string, category: string, quantity: number): void => {
+      setShowQuantityModal(true);
+      setItemName(itemName);
+      setUnits(units);
+      setCurrCategoryName(category);
+      setCurrentQuantity(quantity);
     };
 
     const closeDeleteModal = (): void => {
@@ -304,7 +306,8 @@ export const InventorySpreadsheet: React.FC<InventorySpreadsheetProps> = ({ inve
                           openQuantityModal(
                             String(sortedItems[index][0]),
                             String(sortedItems[index][3]),
-                            String(sortedItems[index][1])
+                            String(sortedItems[index][1]),
+                            Number(sortedItems[index][2])
                           )
                         }
                       />
@@ -324,6 +327,7 @@ export const InventorySpreadsheet: React.FC<InventorySpreadsheetProps> = ({ inve
                               closeModal={closeQuantityModal}
                               handleUpdate={handleUpdateQuantity}
                               categoryName={String(currCategoryName)}
+                              currentQuantity={currentQuantity}
                           />
                       )}
                       <MdOutlineFileDownload
