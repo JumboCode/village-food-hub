b054c7a6a01e2edf0d51e751e2345f9e399fc3d7
Jiyoon Choi <jiyoonchoi15@gmail.com>
2025-04-12 14:55:20 -0400

    cleaned up ProtectedUrls.tsx

--- app/components/ProtectedUrls.tsx
+++ app/components/ProtectedUrls.tsx
@@ -8,22 +8,6 @@ export function userIsStaff(user: UserResource | null | undefined): boolean {
   return !!user && user.publicMetadata?.role === "Staff";
 }
 
-// export function userIsVolunteer(user: UserResource | null | undefined): boolean {
-//   return !!user && user.publicMetadata?.role === "Volunteer";
-// }
-
 export function userIsCustomer(user: UserResource | null | undefined): boolean {
   return !!user && user.publicMetadata?.role === "Customer";
 }
-
-// export function userIsNotVolunteer(user: UserResource | null | undefined): boolean {
-//   return !!user &&
-//     user.username?.toLowerCase() !== "volunteer" &&
-//     user.publicMetadata?.role !== "Volunteer";
-// }
-
-// export function userIsNotCustomer(user: UserResource | null | undefined): boolean {
-//   return !!user &&
-//     user.username?.toLowerCase() !== "customer" &&
-//     user.publicMetadata?.role !== "Customer";
-// }
