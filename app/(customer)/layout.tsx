92608f7fcaaff9d769489f95e21177dc6e62aa7c
Jiyoon Choi <jiyoonchoi15@gmail.com>
2025-04-23 18:24:26 -0400

    trying to get ipad full screen

--- app/(customer)/layout.tsx
+++ app/(customer)/layout.tsx
@@ -13,6 +13,8 @@ export default function CustomerLayout({ children }: { children: ReactNode }) {
     <>
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </Head>
       {children}
     </>
